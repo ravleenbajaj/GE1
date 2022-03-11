5a69182e0abcb2693af7687fbf87b1a4abf6fff2
Ravleen Bajaj <96468459+ravleenbajaj@users.noreply.github.com>
2022-03-11 13:39:45 +0530

    Update Source Codes.py

--- Source Codes.py
+++ Source Codes.py
@@ -237,6 +237,23 @@ def main():
 if __name__=='__main__':
     main()
 
+
+#QUESTION 13
+
+def pallindrome():
+    str = input("Enter a string to check whether it's a palindrome or not  :  ")
+    str = str.lower()
+    length = len(str)-1
+    for i in range(0,length):
+        if  str[i] ==str[length]:
+            length-=1
+            print("string is a pallindrome :) ")
+        else:
+            print("string is not a pallindrome :( ")
+while True:
+        
+    pallindrome()
+
                             
  #QUESTION 14
                             
